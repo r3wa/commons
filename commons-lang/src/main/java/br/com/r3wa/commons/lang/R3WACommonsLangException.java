f21e7c7449e82308046d74150365b4006b16ec1e
Robson <dev@r3wa.com.br>
2015-07-30 16:48:15 -0300

    add the exception to Lib.

---
+++ commons-lang/src/main/java/br/com/r3wa/commons/lang/R3WACommonsLangException.java
@@ -0,0 +1,25 @@
+package br.com.r3wa.commons.lang;
+
+public class R3WACommonsLangException extends RuntimeException{
+
+
+	private static final long serialVersionUID = 1L;
+
+
+	public R3WACommonsLangException() {
+		super();
+	}
+
+
+	public R3WACommonsLangException(String message) {
+		super(message);
+	}
+
+
+	public R3WACommonsLangException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+
+
+}
