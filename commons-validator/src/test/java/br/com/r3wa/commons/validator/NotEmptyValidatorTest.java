dda2e05ebe7a75caecabf05eb9fb9b7eb142a30a
Robson <dev@r3wa.com.br>
2015-08-18 18:51:59 -0300

    refactor test to use ConstraintValidator interface.

--- commons-validator/src/test/java/br/com/r3wa/commons/validator/NotEmptyValidatorTest.java
+++ commons-validator/src/test/java/br/com/r3wa/commons/validator/NotEmptyValidatorTest.java
@@ -3,42 +3,45 @@ package br.com.r3wa.commons.validator;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
 
+import javax.validation.ConstraintValidator;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class NotEmptyValidatorTest {
 
-	private NotEmptyValidator notEmptyValidator;
+
+	private ConstraintValidator<NotEmpty, String> validator;
 
 
 
 	@Before
 	public void setUp() throws Exception {
-		notEmptyValidator = new NotEmptyValidator();
+		validator = new NotEmptyValidator();
 	}
 
 
 
 	@Test
 	public void shouldReturnTrueWhenStringIsNull() {
-		assertThat(notEmptyValidator.isValid(null, null), not(false));
+		assertThat(validator.isValid(null, null), not(false));
 	}
 
 	@Test
 	public void shouldReturnTrueWhenStringIsNotNull() {
-		assertThat(notEmptyValidator.isValid("Robson", null), not(false));
+		assertThat(validator.isValid("Robson", null), not(false));
 	}
 
 
 	@Test
 	public void shouldReturnFalseWhenStringIsEmpty1() {
-		assertThat(notEmptyValidator.isValid("       ", null), not(true));
+		assertThat(validator.isValid("       ", null), not(true));
 	}
 
 
 	@Test
 	public void shouldReturnFalseWhenStringIsEmpty2() {
-		assertThat(notEmptyValidator.isValid("", null), not(true));
+		assertThat(validator.isValid("", null), not(true));
 	}
 
 
