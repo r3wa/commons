687bf661c3dd0d728fc9e04dec9e1a3b28523e2e
Robson <dev@r3wa.com.br>
2015-08-29 10:46:30 -0300

    add new method to create one big decimal.

--- commons-lang/src/main/java/br/com/r3wa/commons/lang/NumberUtils.java
+++ commons-lang/src/main/java/br/com/r3wa/commons/lang/NumberUtils.java
@@ -1,5 +1,6 @@
 package br.com.r3wa.commons.lang;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class NumberUtils {
@@ -9,6 +10,11 @@ public class NumberUtils {
 	public static BigInteger toBigInt(long number) {
 		return BigInteger.valueOf(number);
 	}
+
+	public static BigDecimal toBigDouble(long number) {
+		return new BigDecimal(number);
+	}
+
 
 
 }
--- commons-lang/src/test/java/br/com/r3wa/commons/lang/NumberUtilsTest.java
+++ commons-lang/src/test/java/br/com/r3wa/commons/lang/NumberUtilsTest.java
@@ -3,6 +3,7 @@ package br.com.r3wa.commons.lang;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import org.junit.Test;
@@ -15,6 +16,12 @@ public class NumberUtilsTest {
 	@Test
 	public void shouldCreateOneBigIntByOneLong() {
 		assertThat(NumberUtils.toBigInt(1L), equalTo(new BigInteger("1")));
+	}
+
+
+	@Test
+	public void shouldCreateOneBigDoubleByOneLong() {
+		assertThat(NumberUtils.toBigDouble(1L), equalTo(new BigDecimal(1L)));
 	}
 
 
