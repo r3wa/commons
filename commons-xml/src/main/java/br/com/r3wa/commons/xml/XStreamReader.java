bd96606dfb92fd7c588110a9d7c85d0a71c46421
robsonoduarte <robson.o.d@gmail.com>
2015-09-05 15:50:59 -0300

    starting new xml reader with Xstream

---
+++ commons-xml/src/main/java/br/com/r3wa/commons/xml/XStreamReader.java
@@ -0,0 +1,15 @@
+package br.com.r3wa.commons.xml;
+
+public class XStreamReader implements XMLReader<XML> {
+
+	
+	
+	@Override
+	public XML read(Class<XML> c, String xml) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
